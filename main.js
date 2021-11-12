11b1157261ed34f85b0f8baefb8885dd1ad06bde
HU-M <83540224+HU-M@users.noreply.github.com>
2021-11-12 16:10:04 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,33 @@
+function setup() {
+    video = createCapture(VIDEO);
+    video.size(550,500);
+
+    canvas = createCapture(400, 400);
+    canvas.position(560, 150);
+poseNet = ml5.poseNet(video, modelLoaded);
+}
+
+function modelLoaded() {
+    console.log('PoseNet is Initialized');
+}
+
+function gotPoses(results)
+{
+
+ if (results.length > 0) 
+ {
+  console.log(results);
+  
+  leftWristX = results[0].pose.leftWristX.x;
+  rightWristX = results[0].pose.rightWristX.x;
+  difference = floor(leftWristX - rightWristX);
+ }
+}
+
+function draw() {
+    background('#6C91C2');
+
+    textSize(difference);
+    fill('#FFE787');
+    text('Manush', 50, 400);
+}
